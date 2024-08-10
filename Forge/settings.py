4de91ec50143b817fae36ef3c5471e0cc6de5282
Anthony Grady <a.grady.solutions@gmail.com>
2024-08-10 11:19:54 -0400

    SQL

--- Forge/settings.py
+++ Forge/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'psycopg2',
     'website.apps.WebsiteConfig',
 ]
 
@@ -55,8 +56,7 @@ ROOT_URLCONF = 'Forge.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,14 +74,25 @@ WSGI_APPLICATION = 'Forge.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'forge',
+        'USER': 'postgres',
+        'PASSWORD': 'humbolt637',
+        'HOST': 'forge.cfnbksnzhptb.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
